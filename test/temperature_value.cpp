7a6c7ce102ca4cb32a72a4ec9e2dc2bd072200f2
Denis Koshenkov <dkoshenkov@gmail.com>
2025-07-26 15:31:56 +0200

    Initial formatting

--- test/temperature_value.cpp
+++ test/temperature_value.cpp
@@ -5,24 +5,22 @@ extern "C"
 #include "temperature_value.h"
 }
 
-TEST_GROUP(TemperatureValue)
-{
-};
+TEST_GROUP(TemperatureValue){};
 
 TEST(TemperatureValue, GetWhatWeSetValue1)
 {
-   temperature_value temperature_value;
-   temperature set_temperature = 1;
-   temperature_value_set(&temperature_value, set_temperature);
-   temperature retrieved_temperature = temperature_value_get(&temperature_value);
-   CHECK_EQUAL(set_temperature, retrieved_temperature);
+    temperature_value temperature_value;
+    temperature set_temperature = 1;
+    temperature_value_set(&temperature_value, set_temperature);
+    temperature retrieved_temperature = temperature_value_get(&temperature_value);
+    CHECK_EQUAL(set_temperature, retrieved_temperature);
 }
 
 TEST(TemperatureValue, GetWhatWeSetValue22)
 {
-   temperature_value temperature_value;
-   temperature set_temperature = 22;
-   temperature_value_set(&temperature_value, set_temperature);
-   temperature retrieved_temperature = temperature_value_get(&temperature_value);
-   CHECK_EQUAL(set_temperature, retrieved_temperature);
+    temperature_value temperature_value;
+    temperature set_temperature = 22;
+    temperature_value_set(&temperature_value, set_temperature);
+    temperature retrieved_temperature = temperature_value_get(&temperature_value);
+    CHECK_EQUAL(set_temperature, retrieved_temperature);
 }
