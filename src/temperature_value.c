27fad548bdaaf499638c4486a8337a150cf89a29
Denis Koshenkov <dkoshenkov@gmail.com>
2025-07-26 15:31:56 +0200

    Initial formatting

--- src/temperature_value.c
+++ src/temperature_value.c
@@ -1,9 +1,11 @@
 #include "temperature_value.h"
 
-void temperature_value_set(struct temperature_value *temperature_value, temperature value) {
+void temperature_value_set(struct temperature_value *temperature_value, temperature value)
+{
     temperature_value->value = value;
 }
 
-temperature temperature_value_get(struct temperature_value *temperature_value) {
+temperature temperature_value_get(struct temperature_value *temperature_value)
+{
     return temperature_value->value;
 }
