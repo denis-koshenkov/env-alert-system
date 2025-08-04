28d5afb96cac870d3dd339530a664cf6b8128473
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-04 07:21:27 +0200

    First test using mock running

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,17 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestRegistry.h"
+#include "CppUTestExt/MockSupportPlugin.h"
 #include "CppUTestExt/TestAssertPlugin.h"
 
 int main(int ac, char **av)
 {
+    /* Test assert plugin */
     TestAssertPlugin testAssertPlugin;
     TestRegistry::getCurrentRegistry()->installPlugin(&testAssertPlugin);
+
+    /* Mock support plugin */
+    MockSupportPlugin mockPlugin;
+    TestRegistry::getCurrentRegistry()->installPlugin(&mockPlugin);
+
     return CommandLineTestRunner::RunAllTests(ac, av);
 }
--- test/temperature_value.cpp
+++ test/temperature_value.cpp
@@ -1,10 +1,13 @@
 #include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
 #include "CppUTestExt/TestAssertPlugin.h"
 
 #include "temperature_value.h"
 
 TEST_GROUP(TemperatureValue){};
 
-TEST(TemperatureValue, FirstTest)
+TEST(TemperatureValue, createCallsValueHolderCreateWithSize2)
 {
+    mock().expectOneCall("value_holder_create").withParameter("p2", 2);
+    temperature_value tv = temperature_value_create();
 }
