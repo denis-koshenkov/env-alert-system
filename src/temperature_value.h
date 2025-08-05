ec5f1264819cfcce6ddccfa8306c9c20949a8030
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-05 19:04:40 +0200

    Temperature value module docs

--- src/temperature_value.h
+++ src/temperature_value.h
@@ -10,14 +10,48 @@ extern "C"
 
 #include "hal/temperature.h"
 
+/**
+ * @brief Stores a temperature value and reports when it changes.
+ */
 typedef struct temperature_value_struct *temperature_value;
 
+/**
+ * @brief Create a temperature value instance.
+ *
+ * @return temperature_value Temperature value instance.
+ */
 temperature_value temperature_value_create();
 
+/**
+ * @brief Set temperature value.
+ *
+ * @param tv Temperature value instance returned by @ref temperature_value_create.
+ * @param temperature Temperature value to set.
+ */
 void temperature_value_set(temperature_value tv, temperature temperature);
 
+/**
+ * @brief Get temperature value.
+ *
+ * @pre @ref temperature_value_set has been called at least once.
+ *
+ * @param tv Temperature value instance returned by @ref temperature_value_create.
+ *
+ * @return temperature Temperature value.
+ */
 temperature temperature_value_get(temperature_value tv);
 
+/**
+ * @brief Check whether the temperature value has changed.
+ *
+ * @pre @ref temperature_value_set has been called at least once.
+ *
+ * @param tv Temperature value instance returned by @ref temperature_value_create.
+ *
+ * @return true The last two calls to @ref temperature_value_set had the same value as arguments, or @ref
+ * temperature_value_set has been called only once so far.
+ * @return false The last two calls to @ref temperature_value_set had differing values as arguments.
+ */
 bool temperature_value_is_value_changed(temperature_value tv);
 
 #ifdef __cplusplus
