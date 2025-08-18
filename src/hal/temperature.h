5cf41060c88cff83ab7a14538fddfc1f8c8241f9
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-18 07:57:12 +0200

    Change temperature data type to int16_t
    This allows us to store negative temperature values.

--- src/hal/temperature.h
+++ src/hal/temperature.h
@@ -8,7 +8,24 @@ extern "C"
 
 #include <stdint.h>
 
-typedef uint16_t Temperature;
+/**
+ * @brief Temperature data type definition.
+ *
+ * Temperature is represented in Celsius, with a precision of one decimal point. Negative values are also allowed, hence
+ * we use a signed data type to represent the temperature. Some examples of how various temperature values are
+ * represented:
+ *   - 20.0 degrees -> 200
+ *   - 20.5 degrees -> 205
+ *   - 1.9 degrees -> 19
+ *   - 0.0 degrees -> 0
+ *   - 0.1 degrees -> 1
+ *   - -0.1 degrees -> -1
+ *   - -1.0 degrees -> -10
+ *   - -20.5 degrees -> -205
+ *
+ * Temperature values range allowed by representation: -3276.8 to 3276.7.
+ */
+typedef int16_t Temperature;
 
 #ifdef __cplusplus
 }
