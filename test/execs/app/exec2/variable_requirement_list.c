ca335a899d63b96ffd0e7c8d9e408e3652c9690a
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-30 06:34:17 +0200

    Generic way to verify if all and only expected requirements are in the list

--- test/execs/app/exec2/variable_requirement_list.c
+++ test/execs/app/exec2/variable_requirement_list.c
@@ -6,49 +6,98 @@
  * pressure requirements to add to the list. */
 #include "pressure_requirement.h"
 #include "fake_variable_requirement_allocator.h"
+#include "eas_assert.h"
 
-static VariableRequirement pressure_requirement;
-static void *requirement_buffer;
+#define TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS 1
 
-static VariableRequirement var_req_in_list = NULL;
-static uint8_t count = 0;
-static void for_each_cb(VariableRequirement variable_requirement)
+typedef struct ExpectedVariableRequirement {
+    void *requirement_buffer;
+    VariableRequirement requirement;
+    bool is_expected;
+} ExpectedVariableRequirement;
+
+static ExpectedVariableRequirement expected_requirements[TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS];
+
+static void expect_requirement_in_list(size_t index)
 {
-    var_req_in_list = variable_requirement;
-    count++;
+    EAS_ASSERT(index < TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS);
+    expected_requirements[index].is_expected = true;
+}
+
+static void actual_requirement_detected_in_list(VariableRequirement requirement)
+{
+    for (size_t i = 0; i < TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS; i++) {
+        if (expected_requirements[i].requirement == requirement) {
+            /* Initially all is_expected flags are false. During the test setup step, is_expected flag is set for
+             * all requirements that are expected to be in the list. During the test exercise step, we flip the flags
+             * for all variables that were actually detected in the list. If all and only the required variables were in
+             * the list, all flags will be false by the end of the test. Otherwise, some flags will be true - either
+             * variables that were expected in the list but were not in the list, or variables that were in the list
+             * but were not expected to be there. */
+            expected_requirements[i].is_expected = !expected_requirements[i].is_expected;
+            return;
+        }
+    }
+
+    /* Invalid requirement - it is not in the expected requirements list */
+    EAS_ASSERT(false);
+}
+
+static bool expected_requirements_match_actual()
+{
+    for (size_t i = 0; i < TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS; i++) {
+        if (expected_requirements[i].is_expected) {
+            /* This variable either was expected to be in the list but was not there, or it was not
+             * expected to be in the list but was there. */
+            return false;
+        }
+    }
+    return true;
+}
+
+static void for_each_cb_expected_requirements(VariableRequirement variable_requirement)
+{
+    actual_requirement_detected_in_list(variable_requirement);
 }
 
 TEST_GROUP_C_SETUP(VariableRequirementList)
 {
-    requirement_buffer = fake_variable_requirement_allocator_alloc();
-    mock_c()->expectOneCall("variable_requirement_allocator_alloc")->andReturnPointerValue(requirement_buffer);
-    pressure_requirement = pressure_requirement_create(0, VARIABLE_REQUIREMENT_OPERATOR_GEQ, 0);
-
-    var_req_in_list = NULL;
-    count = 0;
+    for (size_t i = 0; i < TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS; i++) {
+        expected_requirements[i].requirement_buffer = fake_variable_requirement_allocator_alloc();
+        mock_c()
+            ->expectOneCall("variable_requirement_allocator_alloc")
+            ->andReturnPointerValue(expected_requirements[i].requirement_buffer);
+        expected_requirements[i].requirement = pressure_requirement_create(0, VARIABLE_REQUIREMENT_OPERATOR_GEQ, 0);
+        expected_requirements[i].is_expected = false;
+    }
 }
 
 TEST_GROUP_C_TEARDOWN(VariableRequirementList)
 {
-    mock_c()->expectOneCall("variable_requirement_allocator_free")->withPointerParameters("buf", requirement_buffer);
-    variable_requirement_destroy(pressure_requirement);
-    fake_variable_requirement_allocator_free(requirement_buffer);
+    for (size_t i = 0; i < TEST_VARIABLE_REQUIREMENT_LIST_MAX_NUM_EXPECTED_REQUIREMENTS; i++) {
+        mock_c()
+            ->expectOneCall("variable_requirement_allocator_free")
+            ->withPointerParameters("buf", expected_requirements[i].requirement_buffer);
+        variable_requirement_destroy(expected_requirements[i].requirement);
+        fake_variable_requirement_allocator_free(expected_requirements[i].requirement_buffer);
+    }
 }
 
 TEST_C(VariableRequirementList, ListIsEmptyAfterCreate)
 {
     VariableRequirementList list = variable_requirement_list_create();
-    variable_requirement_list_for_each(list, for_each_cb);
-    CHECK_C(var_req_in_list == NULL);
-    CHECK_EQUAL_C_UINT(0, count);
+    variable_requirement_list_for_each(list, for_each_cb_expected_requirements);
+
+    CHECK_C(expected_requirements_match_actual());
 }
 
 TEST_C(VariableRequirementList, ListContainsOneVarWhenOneVarAdded)
 {
-    VariableRequirementList list = variable_requirement_list_create();
-    variable_requirement_list_add(list, pressure_requirement);
-    variable_requirement_list_for_each(list, for_each_cb);
+    expect_requirement_in_list(0);
 
-    CHECK_EQUAL_C_POINTER(pressure_requirement, var_req_in_list);
-    CHECK_EQUAL_C_UINT(1, count);
+    VariableRequirementList list = variable_requirement_list_create();
+    variable_requirement_list_add(list, expected_requirements[0].requirement);
+    variable_requirement_list_for_each(list, for_each_cb_expected_requirements);
+
+    CHECK_C(expected_requirements_match_actual());
 }
