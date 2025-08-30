98d9ce54ab56fbd8b0dc54e39d4d9e0fd5d6c857
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-30 11:53:10 +0200

    Temperature requirement list docs

--- src/temperature_requirement_list.h
+++ src/temperature_requirement_list.h
@@ -10,10 +10,32 @@ extern "C"
 
 #include "variable_requirement_list_defs.h"
 
+/**
+ * @brief Stores temperature requirements of all currently registered alerts in one list.
+ */
+
+/**
+ * @brief Add a temperature requirement to the list.
+ *
+ * @param variable_requirement Temperature requirement to add to the list.
+ */
 void temperature_requirement_list_add(VariableRequirement temperature_requirement);
 
+/**
+ * @brief Execute a callback for each temperature requirement in the list.
+ *
+ * @param cb Callback to execute for each temperature requirement currently in the list.
+ *
+ * @note Fires an assert if @p cb is NULL. Calling this function with @p cb equal to NULL would be equivalent to not
+ * calling this function at all.
+ */
 void temperature_requirement_list_for_each(VariableRequirementListForEachCb cb);
 
+/**
+ * @brief Remove all temperature requirements from the list that belong to a specific alert.
+ *
+ * @param alert_id All variable requirements that belong to the alert with this id are removed from the list.
+ */
 void temperature_requirement_list_remove_all_for_alert(uint8_t alert_id);
 
 #ifdef __cplusplus
