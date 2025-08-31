0bed52a540dc7875b0aa138824d43199d16aba9b
Denis Koshenkov <dkoshenkov@gmail.com>
2025-08-31 20:05:55 +0200

    alert_condition_get returns the instance returned by set

--- src/alert_conditions.c
+++ src/alert_conditions.c
@@ -3,12 +3,16 @@
 #include "alert_conditions.h"
 #include "eas_assert.h"
 
+static AlertCondition alert_condition_instance = NULL;
+
 void alert_conditions_set_alert_condition(uint8_t alert_id, AlertCondition alert_condition)
 {
+    EAS_ASSERT(alert_condition);
+    alert_condition_instance = alert_condition;
 }
 
 AlertCondition alert_conditions_get_alert_condition(uint8_t alert_id)
 {
-    EAS_ASSERT(0);
-    return NULL;
+    EAS_ASSERT(alert_condition_instance);
+    return alert_condition_instance;
 }
--- test/execs/app/exec2/alert_conditions.cpp
+++ test/execs/app/exec2/alert_conditions.cpp
@@ -8,7 +8,24 @@ TEST_GROUP(AlertConditions){};
 
 TEST_ORDERED(AlertConditions, GetAssertsIfCalledBeforeSet, 0)
 {
-    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("0", "alert_conditions_get_alert_condition");
+    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("alert_condition_instance", "alert_conditions_get_alert_condition");
 
     AlertCondition alert_condition_0 = alert_conditions_get_alert_condition(0);
+}
+
+TEST_ORDERED(AlertConditions, SetAssertsIfConditionIsNull, 1)
+{
+    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("alert_condition", "alert_conditions_set_alert_condition");
+
+    alert_conditions_set_alert_condition(0, NULL);
+}
+
+TEST_ORDERED(AlertConditions, GetWhatWeSetAlertId0, 1)
+{
+    /* We are only checking that the same instance is returned - no need to use a real instance */
+    AlertCondition expected_alert_condition = (AlertCondition)0x4926;
+
+    alert_conditions_set_alert_condition(0, expected_alert_condition);
+    AlertCondition actual_alert_condition = alert_conditions_get_alert_condition(0);
+    CHECK_EQUAL(expected_alert_condition, actual_alert_condition);
 }
