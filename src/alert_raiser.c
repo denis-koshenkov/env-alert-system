f11f9d74737b8b51b79a8ebf2181141d7b292e60
Denis Koshenkov <dkoshenkov@gmail.com>
2025-09-01 10:18:16 +0200

    Warmup and cooldown 0 notify immediately

--- src/alert_raiser.c
+++ src/alert_raiser.c
@@ -3,6 +3,7 @@
 #include "alert_raiser.h"
 #include "alert_notifier.h"
 #include "eas_assert.h"
+#include "config.h"
 
 #ifndef CONFIG_ALERT_RAISER_MAX_NUM_INSTANCES
 #define CONFIG_ALERT_RAISER_MAX_NUM_INSTANCES 1
@@ -28,5 +29,5 @@ void alert_raiser_set_alert(AlertRaiser self, uint8_t alert_id, uint32_t warmup_
 
 void alert_raiser_set_alert_condition_result(AlertRaiser self, bool alert_condition_result)
 {
-    alert_notifier_notify(0, true);
+    alert_notifier_notify(0, alert_condition_result);
 }
--- test/execs/app/exec2/alert_raiser.cpp
+++ test/execs/app/exec2/alert_raiser.cpp
@@ -16,3 +16,17 @@ TEST(AlertRaiser, Warmup0ImmediatelyNotifiesWhenAlertConditionBecomesTrue)
     alert_raiser_set_alert(alert_raiser, alert_id, warmup_period, cooldown_period);
     alert_raiser_set_alert_condition_result(alert_raiser, true);
 }
+
+TEST(AlertRaiser, Warmup0Cooldown0ImmediatelyNotify)
+{
+    uint8_t alert_id = 0;
+    uint32_t warmup_period = 0;
+    uint32_t cooldown_period = 0;
+    mock().expectOneCall("alert_notifier_notify").withParameter("alert_id", alert_id).withParameter("is_raised", true);
+    mock().expectOneCall("alert_notifier_notify").withParameter("alert_id", alert_id).withParameter("is_raised", false);
+
+    AlertRaiser alert_raiser = alert_raiser_create();
+    alert_raiser_set_alert(alert_raiser, alert_id, warmup_period, cooldown_period);
+    alert_raiser_set_alert_condition_result(alert_raiser, true);
+    alert_raiser_set_alert_condition_result(alert_raiser, false);
+}
