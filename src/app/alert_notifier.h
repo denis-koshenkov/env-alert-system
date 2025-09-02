398dc4e2d1264e8cd8d8bc38093622bd651780fd
Denis Koshenkov <dkoshenkov@gmail.com>
2025-09-02 14:08:46 +0200

    Add to alert_notifier docs

--- src/app/alert_notifier.h
+++ src/app/alert_notifier.h
@@ -18,6 +18,16 @@ extern "C"
  * AlertRaiser calls this function and expects that all necessary notifications that the alert is now raised or silenced
  * will be executed. It is the responsibility of the implementation of this interface to ensure that.
  *
+ * Each call to this function actually sends all registered notifications. This function should only be called if the
+ * alert is raised or silenced. For example, it is not allowed to do the following:
+ * ```
+ * alert_notifier_notify(0, true);
+ * alert_notifier_notify(0, true);
+ * ```
+ * The first call will send the notifications that the alert with id 0 is now raised. The second call will send the same
+ * notifications again - and this should not happen. Notifications should only be sent when the alert state changes. It
+ * is therefore the responsibility of the caller of this function to ensure that this situation does not occur.
+ *
  * @param alert_id Alert id of the alert to be raised or silenced.
  * @param is_raised If true, execute notifications that the alert is raised. Otherwise, execute notifications that the
  * alert is silenced.
