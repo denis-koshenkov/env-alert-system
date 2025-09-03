2f41915f910f1deff3d982eb007281867b897ddd
Denis Koshenkov <dkoshenkov@gmail.com>
2025-09-03 07:54:30 +0200

    Led notifier module

--- src/app/led_notifier.c
+++ src/app/led_notifier.c
@@ -1,6 +1,7 @@
 #include "led_notifier.h"
 #include "led_notification_executor.h"
 #include "config.h"
+#include "eas_assert.h"
 
 #ifndef CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS
 #define CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS 1
@@ -11,25 +12,39 @@ typedef struct AlertLedNotification {
     uint8_t led_color;
     /**< Use values from LedPattern enum, defined as uint8_t to save memory. */
     uint8_t led_pattern;
+    /**< Whether led notification is enabled for this alert. */
+    bool is_enabled;
 } AlertLedNotification;
 
-static bool notifications_enabled = false;
-static AlertLedNotification alert_led_notification;
+static AlertLedNotification alert_led_notifications[CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS];
+
+static bool is_valid_alert_id(uint8_t alert_id)
+{
+    return (alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
+}
 
 void led_notifier_notify(uint8_t alert_id, bool is_raised)
 {
-    if (notifications_enabled) {
-        led_notification_executor_execute(alert_led_notification.led_color, alert_led_notification.led_pattern, true);
+    EAS_ASSERT(is_valid_alert_id(alert_id));
+
+    if (alert_led_notifications[alert_id].is_enabled) {
+        led_notification_executor_execute(alert_led_notifications[alert_id].led_color,
+                                          alert_led_notifications[alert_id].led_pattern, is_raised);
     }
 }
 
 void led_notifier_enable_notifications(uint8_t alert_id, LedColor led_color, LedPattern led_pattern)
 {
-    alert_led_notification.led_color = led_color;
-    alert_led_notification.led_pattern = led_pattern;
-    notifications_enabled = true;
+    EAS_ASSERT(is_valid_alert_id(alert_id));
+
+    alert_led_notifications[alert_id].led_color = led_color;
+    alert_led_notifications[alert_id].led_pattern = led_pattern;
+    alert_led_notifications[alert_id].is_enabled = true;
 }
 
 void led_notifier_disable_notifications(uint8_t alert_id)
 {
+    EAS_ASSERT(is_valid_alert_id(alert_id));
+
+    alert_led_notifications[alert_id].is_enabled = false;
 }
--- src/port/unit-test-off-target/test/execs/app/exec2/led_notifier.cpp
+++ src/port/unit-test-off-target/test/execs/app/exec2/led_notifier.cpp
@@ -1,6 +1,7 @@
 #include "CppUTest/TestHarness.h"
 #include "CppUTestExt/OrderedTest.h"
 #include "CppUTestExt/MockSupport.h"
+#include "CppUTestExt/TestAssertPlugin.h"
 
 #include "led_notifier.h"
 #include "config.h"
@@ -19,12 +20,12 @@ TEST_ORDERED(LedNotifier, AllNotificationsInitiallyDisabled, 0)
     }
 }
 
-TEST_ORDERED(LedNotifier, EnabledNotificationCallsSenderId0, 1)
+TEST_ORDERED(LedNotifier, EnabledNotificationCallsExecutorId0, 1)
 {
     uint8_t alert_id = 0;
     LedColor led_color = LED_COLOR_RED;
     LedPattern led_pattern = LED_PATTERN_STATIC;
-    EAS_ASSERT(alert_id < CONFIG_CONNECTIVITY_NOTIFIER_MAX_NUM_ALERTS);
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
 
     mock()
         .expectOneCall("led_notification_executor_execute")
@@ -40,12 +41,12 @@ TEST_ORDERED(LedNotifier, EnabledNotificationCallsSenderId0, 1)
     led_notifier_disable_notifications(alert_id);
 }
 
-TEST_ORDERED(LedNotifier, EnabledNotificationCallsSenderId2, 1)
+TEST_ORDERED(LedNotifier, EnabledNotificationCallsExecutorId2, 1)
 {
     uint8_t alert_id = 2;
     LedColor led_color = LED_COLOR_GREEN;
     LedPattern led_pattern = LED_PATTERN_ALERT;
-    EAS_ASSERT(alert_id < CONFIG_CONNECTIVITY_NOTIFIER_MAX_NUM_ALERTS);
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
 
     mock()
         .expectOneCall("led_notification_executor_execute")
@@ -59,4 +60,103 @@ TEST_ORDERED(LedNotifier, EnabledNotificationCallsSenderId2, 1)
 
     /* Clean up */
     led_notifier_disable_notifications(alert_id);
+}
+
+TEST_ORDERED(LedNotifier, DisabledNotificationDoesNotCallExecutor, 1)
+{
+    uint8_t alert_id = 1;
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
+
+    /* This should not call led_notification_executor_execute, since notifications are disabled */
+    led_notifier_notify(alert_id, true);
+}
+
+TEST_ORDERED(LedNotifier, IsRaisedPropagatedToExecutorCall, 1)
+{
+    uint8_t alert_id = 0;
+    LedColor led_color = LED_COLOR_BLUE;
+    LedPattern led_pattern = LED_PATTERN_STATIC;
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
+
+    mock()
+        .expectOneCall("led_notification_executor_execute")
+        .withParameter("led_color", led_color)
+        .withParameter("led_pattern", led_pattern)
+        .withParameter("should_be_displayed", true);
+    mock()
+        .expectOneCall("led_notification_executor_execute")
+        .withParameter("led_color", led_color)
+        .withParameter("led_pattern", led_pattern)
+        .withParameter("should_be_displayed", false);
+
+    led_notifier_enable_notifications(alert_id, led_color, led_pattern);
+    /* Calls led_notification_executor_execute with should_be_displayed == true */
+    led_notifier_notify(alert_id, true);
+    /* Calls led_notification_executor_execute with should_be_displayed == false */
+    led_notifier_notify(alert_id, false);
+
+    /* Clean up */
+    led_notifier_disable_notifications(alert_id);
+}
+
+TEST_ORDERED(LedNotifier, DisablePreviouslyEnabledNotificationDoesNotCallExecutor, 1)
+{
+    uint8_t alert_id = 1;
+    LedColor led_color = LED_COLOR_RED;
+    LedPattern led_pattern = LED_PATTERN_ALERT;
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
+
+    led_notifier_enable_notifications(alert_id, led_color, led_pattern);
+    led_notifier_disable_notifications(alert_id);
+    /* This should not call led_notification_executor_execute, since notifications are now disabled */
+    led_notifier_notify(alert_id, true);
+}
+
+TEST_ORDERED(LedNotifier, EnablePreviouslyDisabledNotificationCallsExecutor, 1)
+{
+    uint8_t alert_id = 0;
+    LedColor led_color = LED_COLOR_GREEN;
+    LedPattern led_pattern = LED_PATTERN_STATIC;
+    EAS_ASSERT(alert_id < CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS);
+
+    mock()
+        .expectOneCall("led_notification_executor_execute")
+        .withParameter("led_color", led_color)
+        .withParameter("led_pattern", led_pattern)
+        .withParameter("should_be_displayed", true);
+
+    led_notifier_enable_notifications(alert_id, led_color, led_pattern);
+    led_notifier_disable_notifications(alert_id);
+    led_notifier_enable_notifications(alert_id, led_color, led_pattern);
+    /* Should call led_notification_executor_execute */
+    led_notifier_notify(alert_id, true);
+
+    /* Clean up */
+    led_notifier_disable_notifications(alert_id);
+}
+
+TEST_ORDERED(LedNotifier, EnableNotificationAssertsIfAlertIdInvalid, 1)
+{
+    uint8_t alert_id = CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS;
+    LedColor led_color = LED_COLOR_BLUE;
+    LedPattern led_pattern = LED_PATTERN_ALERT;
+    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("is_valid_alert_id(alert_id)", "led_notifier_enable_notifications");
+
+    led_notifier_enable_notifications(alert_id, led_color, led_pattern);
+}
+
+TEST_ORDERED(LedNotifier, DisableNotificationAssertsIfAlertIdInvalid, 1)
+{
+    uint8_t alert_id = CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS;
+    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("is_valid_alert_id(alert_id)", "led_notifier_disable_notifications");
+
+    led_notifier_disable_notifications(alert_id);
+}
+
+TEST_ORDERED(LedNotifier, NotifyAssertsIfAlertIdInvalid, 1)
+{
+    uint8_t alert_id = CONFIG_LED_NOTIFIER_MAX_NUM_ALERTS;
+    TEST_ASSERT_PLUGIN_EXPECT_ASSERTION("is_valid_alert_id(alert_id)", "led_notifier_notify");
+
+    led_notifier_notify(alert_id, false);
 }
