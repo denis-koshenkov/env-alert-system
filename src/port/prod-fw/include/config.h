c5244995624be1103fce324ffb36e8d6f01e74a4
Denis Koshenkov <dkoshenkov@gmail.com>
2025-09-11 18:37:04 +0200

    Document number of linked list nodes needed in prod fw

--- src/port/prod-fw/include/config.h
+++ src/port/prod-fw/include/config.h
@@ -14,4 +14,14 @@
  */
 #define CONFIG_VARIABLE_REQUIREMENT_MAX_SIZE 1
 
+/** The maximal number of nodes that the linked list node allocator module can allocate at the same time.
+ * We need CONFIG_MAX_NUM_ALERTS nodes for LedManager - one node for each added LedNotification. We also need nodes for
+ * variable requirements. We also need nodes for variable requirements, probably 100 - (CONFIG_MAX_NUM_ALERTS *
+ * CONFIG_MAX_NUM_REQUIREMENTS_PER_ALERT), where CONFIG_MAX_NUM_REQUIREMENTS_PER_ALERT is 10. This way, we can always
+ * store all variable requirements.
+ *
+ * TODO: setting this to one to not have enough nodes so that we adjust it according to the calculation.
+ */
+#define CONFIG_LINKED_LIST_NODE_ALLOCATOR_NUM_NODES 1
+
 #endif /* ENV_ALERT_SYSTEM_SRC_PORT_PROD_FW_INCLUDE_CONFIG_H */
