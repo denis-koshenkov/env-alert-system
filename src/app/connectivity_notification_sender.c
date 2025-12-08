ed222e731f5c7b52e37f6f8c603088919cc3a2ef
Denis Koshenkov <dkoshenkov@gmail.com>
2025-12-08 20:14:51 +0100

    connectivity_notification_sender: Calls msg_transceiver to send the notification

--- src/app/connectivity_notification_sender.c
+++ src/app/connectivity_notification_sender.c
@@ -1,7 +1,7 @@
 #include "connectivity_notification_sender.h"
+#include "msg_transceiver.h"
 
 void connectivity_notification_sender_send(uint8_t alert_id, bool is_raised)
 {
-    /* Empty for now, because unit tests mock this interface instead of using the real one (this one). The
-     * implementation here should call a connectivity protocol API to actually transmit the notification message. */
+    msg_transceiver_send_alert_status_change_message(alert_id, is_raised, NULL, NULL);
 }
