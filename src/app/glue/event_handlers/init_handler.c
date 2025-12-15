70588f25db2a478307e7cb56549687db0046faf7
Denis Koshenkov <dkoshenkov@gmail.com>
2025-12-15 07:04:28 +0100

    Separate dir for event handlers

---
+++ src/app/glue/event_handlers/init_handler.c
@@ -0,0 +1,5 @@
+#include "init_handler.h"
+
+void init_handler_handle_init_event()
+{
+}
---
+++ src/app/glue/event_handlers/init_handler.h
@@ -0,0 +1,15 @@
+#ifndef ENV_ALERT_SYSTEM_SRC_APP_GLUE_EVENT_HANDLERS_INIT_HANDLER_H
+#define ENV_ALERT_SYSTEM_SRC_APP_GLUE_EVENT_HANDLERS_INIT_HANDLER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void init_handler_handle_init_event();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ENV_ALERT_SYSTEM_SRC_APP_GLUE_EVENT_HANDLERS_INIT_HANDLER_H */
