52944f2eb5d59e203482e6f429ae6a5a69e8fd8e
Denis Koshenkov <dkoshenkov@gmail.com>
2025-12-16 19:24:49 +0100

    eas_assert: prod-fw port implementation

--- src/port/prod-fw/include/eas_assert.h
+++ src/port/prod-fw/include/eas_assert.h
@@ -1,7 +1,8 @@
 #ifndef ENV_ALERT_SYSTEM_SRC_PORT_PROD_FW_INCLUDE_EAS_ASSERT_H
 #define ENV_ALERT_SYSTEM_SRC_PORT_PROD_FW_INCLUDE_EAS_ASSERT_H
 
-// TODO: decide what EAS_ASSERT does when we are in prod fw
-#define EAS_ASSERT(expr) ((expr) ? (void)(0) : (void)(0))
+#include <zephyr/sys/__assert.h>
+
+#define EAS_ASSERT(expr) __ASSERT_NO_MSG(expr)
 
 #endif /* ENV_ALERT_SYSTEM_SRC_PORT_PROD_FW_INCLUDE_EAS_ASSERT_H */
