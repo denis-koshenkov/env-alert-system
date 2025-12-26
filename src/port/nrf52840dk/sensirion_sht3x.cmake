cf6362bd8caf62b1de9cdb6229cb37f39930e1e0
Denis Koshenkov <dkoshenkov@gmail.com>
2025-12-26 06:02:40 +0100

    Add external SHT3X driver to build

---
+++ src/port/nrf52840dk/sensirion_sht3x.cmake
@@ -0,0 +1,11 @@
+add_library(sht3x_driver INTERFACE)
+
+# Convert to absoluate path to remove /../../.. from the path
+cmake_path(SET SENSIRION_SHT3X_SRC_DIR NORMALIZE "${CMAKE_CURRENT_SOURCE_DIR}/../../../../sensirion-sht3x/src")
+target_sources(sht3x_driver INTERFACE
+    "${SENSIRION_SHT3X_SRC_DIR}/sht3x.c"
+)
+
+target_include_directories(sht3x_driver INTERFACE
+    "${SENSIRION_SHT3X_SRC_DIR}"
+)
