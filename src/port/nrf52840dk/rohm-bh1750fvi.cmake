0c2bb163b95d16c335ebe4459445937121af36af
Denis Koshenkov <dkoshenkov@gmail.com>
2026-01-11 07:34:25 +0100

    Add BH1750 driver to build

---
+++ src/port/nrf52840dk/rohm-bh1750fvi.cmake
@@ -0,0 +1,11 @@
+add_library(bh1750_driver INTERFACE)
+
+# Convert to absoluate path to remove /../../.. from the path
+cmake_path(SET ROHM_BH1750FVI_SRC_DIR NORMALIZE "${CMAKE_CURRENT_SOURCE_DIR}/../../../../rohm-bh1750fvi/src")
+target_sources(bh1750_driver INTERFACE
+    "${ROHM_BH1750FVI_SRC_DIR}/bh1750.c"
+)
+
+target_include_directories(bh1750_driver INTERFACE
+    "${ROHM_BH1750FVI_SRC_DIR}"
+)
