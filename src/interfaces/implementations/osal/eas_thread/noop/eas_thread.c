e7ec27a66f4e504493c720611fb69eba76cacc2f
Denis Koshenkov <dkoshenkov@gmail.com>
2026-01-16 08:19:33 +0100

    eas_thread: Move to interfaces dir

---
+++ src/interfaces/implementations/osal/eas_thread/noop/eas_thread.c
@@ -0,0 +1,5 @@
+#include "osal/eas_thread.h"
+
+void eas_thread_create(EasThreadRunFunction run_function)
+{
+}
--- src/port/unit-test-off-target/interfaces/osal/eas_thread.c
+++
@@ -1,7 +0,0 @@
-#include "osal/eas_thread.h"
-
-/* No implementation - when the application runs on unit test port, threads are not used. */
-
-void eas_thread_create(EasThreadRunFunction run_function)
-{
-}
