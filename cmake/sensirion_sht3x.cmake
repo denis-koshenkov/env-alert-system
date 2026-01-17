6b3499cb5670ae0bfeb427104d0a474b4deab633
Denis Koshenkov <dkoshenkov@gmail.com>
2026-01-17 08:59:03 +0100

    Move hal definition to hal directory

--- cmake/sensirion_sht3x.cmake
+++ cmake/sensirion_sht3x.cmake
@@ -1,7 +1,7 @@
 add_library(sht3x_driver INTERFACE)
 
 # Convert to absoluate path to remove /../../.. from the path
-cmake_path(SET SENSIRION_SHT3X_SRC_DIR NORMALIZE "${CMAKE_CURRENT_SOURCE_DIR}/../../../../sensirion-sht3x/src")
+cmake_path(SET SENSIRION_SHT3X_SRC_DIR NORMALIZE "${CMAKE_SOURCE_DIR}/../sensirion-sht3x/src")
 target_sources(sht3x_driver INTERFACE
     "${SENSIRION_SHT3X_SRC_DIR}/sht3x.c"
 )
