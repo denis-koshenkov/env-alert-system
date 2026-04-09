a18273a9b918e17d332747a706ac874d21117316
Denis Koshenkov <dkoshenkov@gmail.com>
2026-04-09 07:51:59 +0200

    led_controller: alert pattern

--- src/app/led_controller.c
+++ src/app/led_controller.c
@@ -1,5 +1,102 @@
 #include "led_controller.h"
 #include "hw_platform.h"
+#include "eas_timer.h"
+
+#define LED_CONTROLLER_ALERT_PATTERN_TIMER_PERIOD_MS 300
+
+typedef struct {
+    void (*start)(LedColor color);
+    void (*stop)();
+} LedControllerPattern;
+
+/*--------------------------- Static pattern ----------------------------------------- */
+
+static void static_pattern_start(LedColor color)
+{
+    hw_platform_get_led()->set(color);
+}
+
+static void static_pattern_stop()
+{
+}
+
+static LedControllerPattern static_pattern = {
+    .start = static_pattern_start,
+    .stop = static_pattern_stop,
+};
+
+/*--------------------------- Alert pattern ----------------------------------------- */
+
+static bool is_led_on = false;
+static LedColor alert_color;
+
+static void alert_timer_cb(void *user_data)
+{
+    if (is_led_on) {
+        hw_platform_get_led()->turn_off();
+    } else {
+        hw_platform_get_led()->set(alert_color);
+    }
+    is_led_on = !is_led_on;
+}
+
+/**
+ * @brief Get timer instance. Used for switching between led on and off during the alert pattern.
+ *
+ * @return EasTimer Timer instance.
+ */
+static EasTimer get_timer_instance()
+{
+    static EasTimer instance;
+    static bool is_created = false;
+    if (!is_created) {
+        instance =
+            eas_timer_create(LED_CONTROLLER_ALERT_PATTERN_TIMER_PERIOD_MS, EAS_TIMER_PERIODIC, alert_timer_cb, NULL);
+        is_created = true;
+    }
+    return instance;
+}
+
+static void alert_pattern_start(LedColor color)
+{
+    hw_platform_get_led()->set(color);
+    is_led_on = true;
+    alert_color = color;
+    eas_timer_start(get_timer_instance());
+}
+
+static void alert_pattern_stop()
+{
+}
+
+static LedControllerPattern alert_pattern = {
+    .start = alert_pattern_start,
+    .stop = alert_pattern_stop,
+};
+
+/**
+ * @brief Return LedControllerPatterm corresponding to LedPattern.
+ *
+ * @param[in] pattern Led pattern.
+ *
+ * @return LedControllerPattern* Corresponding LedControllerPattern. NULL if LedPattern is invalid.
+ */
+static LedControllerPattern *led_pattern_to_controller_pattern(LedPattern pattern)
+{
+    LedControllerPattern *controller_pattern = NULL;
+    switch (pattern) {
+    case LED_PATTERN_STATIC:
+        controller_pattern = &static_pattern;
+        break;
+    case LED_PATTERN_ALERT:
+        controller_pattern = &alert_pattern;
+        break;
+    default:
+        controller_pattern = NULL;
+        break;
+    }
+    return controller_pattern;
+}
 
 void led_controller_turn_off()
 {
@@ -8,5 +105,6 @@ void led_controller_turn_off()
 
 void led_controller_set_color_pattern(LedColor color, LedPattern pattern)
 {
-    hw_platform_get_led()->set(color);
+    LedControllerPattern *controller_pattern = led_pattern_to_controller_pattern(pattern);
+    controller_pattern->start(color);
 }
--- test/execs/exec2/led_controller.cpp
+++ test/execs/exec2/led_controller.cpp
@@ -2,8 +2,32 @@
 #include "CppUTestExt/MockSupport.h"
 
 #include "led_controller.h"
+#include "eas_timer_defs.h"
 
-TEST_GROUP(LedController){};
+#define LED_CONTROLLER_TEST_TIMER_PERIOD 300
+
+/* The implementation of eas_timer_create in EasTimer mock object populates these with the timer callback and its user
+ * data. This is needed in the test so that we can call the callback to simulate the timer expiration. */
+static EasTimerCb timer_cb;
+static void *timer_cb_user_data;
+
+static EasTimer timer = (EasTimer)0x28;
+
+// clang-format off
+TEST_GROUP(LedController)
+{
+    void setup() {
+        /* Order of expected calls is important for these tests. Fail the test if the expected mock calls do not happen
+        in the specified order. */
+        mock().strictOrder();
+
+        /* Pass pointers so that the mock object populates them with timer callback and user data */
+        mock().setData("timerCbs", (void *)&timer_cb);
+        mock().setData("timerCbsUserData", &timer_cb_user_data);
+        mock().setData("numTimerCbs", (unsigned int)1);        
+    }
+};
+// clang-format on
 
 TEST(LedController, TurnOffSetsLedToOff)
 {
@@ -21,4 +45,28 @@ TEST(LedController, SetStaticPatternSetLedGreen)
 {
     mock().expectOneCall("led_set").withParameter("led_color", LED_COLOR_GREEN);
     led_controller_set_color_pattern(LED_COLOR_GREEN, LED_PATTERN_STATIC);
+}
+
+TEST(LedController, AlertPatternRed)
+{
+    /* Called by led_controller_set_color_pattern */
+    mock().expectOneCall("led_set").withParameter("led_color", LED_COLOR_RED);
+    mock()
+        .expectOneCall("eas_timer_create")
+        .withParameter("period_ms", LED_CONTROLLER_TEST_TIMER_PERIOD)
+        .withParameter("periodic", true)
+        .ignoreOtherParameters()
+        .andReturnValue(timer);
+    mock().expectOneCall("eas_timer_start").withParameter("self", timer);
+    /* Called by timer_cb */
+    mock().expectOneCall("led_turn_off");
+    /* Called by timer_cb */
+    mock().expectOneCall("led_set").withParameter("led_color", LED_COLOR_RED);
+    /* Called by timer_cb */
+    mock().expectOneCall("led_turn_off");
+
+    led_controller_set_color_pattern(LED_COLOR_RED, LED_PATTERN_ALERT);
+    timer_cb(timer_cb_user_data);
+    timer_cb(timer_cb_user_data);
+    timer_cb(timer_cb_user_data);
 }
